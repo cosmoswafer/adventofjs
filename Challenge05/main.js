fce92006d2f9a6a0be4af318e07019f732858333
Jimmy NGAI <jimmyngai@ssm.gov.mo>
2022-03-17 18:28:42 +0800

    Suspend

--- Challenge05/main.js
+++ Challenge05/main.js
@@ -7,13 +7,45 @@ const content_list = [
   "Freelancing (Part 1)",
 ];
 
+let last_clicked = -1;
+let hold_shift = false;
+document.addEventListener("keydown", e => {
+    e.key == "Shift" && (hold_shift = true);
+});
+
+document.addEventListener("keyup", e => {
+    e.key == "Shift" && (hold_shift = false);
+});
+
 //Building the episodes list
 const template_li = document.querySelector(".episodes ol li.template");
 template_li.style.display = "none";
-for (let content of content_list) {
+for (let i=0; i<content_list.length; i++) {
+  const content = content_list[i];
   const new_li = template_li.cloneNode(true);
   template_li.before(new_li);
   new_li.style.display = "";
   new_li.querySelector("p").textContent = content;
+  new_li.querySelector("p").index = i;
 }
 
+const toggleCheckbox = (a, b) => {
+    console.log(`${a} -> ${b}`);
+}
+
+const clickCheckbox = index => {
+    if (last_clicked != -1 && index != -1) {
+        toggleCheckbox([index, last_clicked].sort());
+    }
+}
+
+const episodes_checkboxes = document.querySelectorAll(".episodes .list ol li p");
+for (let checkbox of episodes_checkboxes) {
+    checkbox.addEventListener("click", e => {
+        if (hold_shift) {
+            console.dir(e.target);
+            clickCheckbox(e.target?.index ?? -1);
+        }
+        last_clicked = e.target?.index ?? -1;
+    });
+}
