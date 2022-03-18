c474a73def0d7720288af7e2c62f625ec084b8a8
Jimmy <jimmy09140456@gmail.com>
2022-03-19 02:45:27 +0800

    Finished JS challenge

--- Challenge07/README.md
+++ Challenge07/README.md
@@ -14,4 +14,18 @@
 
 ### CSS
 
+Haveily used grid layout in this CSS project.
+Front size are set with rem unit.
+Some spacing are relative by using em or rem unit.
+Using relative design will be better if users scale the web page, the elements will be sitting on the right position.
+
+The radio buttons are done by similar technic in previous challenge.
+We hide the radio input, and make the lable looks like a button.
+
+I forgot to use input element for the bill amount and number of people input.
+Thus I set the span element editable and just let it stay there.
+
 ### JavaScript
+
+Pretty easy and straitforward. Nothing special.
+Converting the text from input string to numbers, and put them back in two decimal place, by the `toFixed()` API.
--- Challenge07/main.js
+++ Challenge07/main.js
@@ -1,13 +1,26 @@
+const tip_amount = document.querySelector('.tip-amount span.value');
+const total_per_person = document.querySelector('.total-per-person span.value');
 const bill_amount = document.querySelector('.input-number .bill-amount span');
 const people_number = document.querySelector(
     '.input-number .people-number span'
 );
-const radio_btn = document.querySelector('input[name="percentage"]');
 const cal_btn = document.querySelector('.calculate button');
 
+const update_page = (tip, total_per) => {
+    tip_amount.textContent = tip.toFixed(2);
+    total_per_person.textContent = total_per.toFixed(2);
+};
+
 cal_btn.addEventListener('click', (e) => {
-    console.log({
-        bill_amount: bill_amount.textContent,
-        people_number: people_number.textContent,
-    });
+    const radio_btn = document.querySelector(
+        'input[name="percentage"]:checked'
+    );
+    const bill = Number(bill_amount.textContent);
+    const percentage = Number(radio_btn.value);
+    const person = Number(people_number.textContent);
+
+    const tip = bill * percentage;
+    const total_per = (bill + tip) / person;
+
+    update_page(tip, total_per);
 });
