67de77b7f1303164b7c9d800fa3cce8be45e73b7
Jimmy <jimmy09140456@gmail.com>
2022-03-22 01:19:08 +0800

    Using dom methods not innerHTML.

---
+++ Challenge08/dom.js
@@ -0,0 +1,42 @@
+export class DOM {
+    dom_element = null;
+
+    constructor(node) {
+        this.dom_element = node;
+    }
+
+    _firstByClass(name) {
+        return this?.dom_element.querySelector(`.${name}`);
+    }
+
+    _allByClass(name) {
+        return this?.dom_element.querySelectorAll(`.${name}`);
+    }
+
+    dot(key) {
+        return this._firstByClass(key);
+    }
+
+    getText(key) {
+        return this._firstByClass(key)?.textContent;
+    }
+
+    setText(key, txt) {
+        const target = this._firstByClass(key);
+        if (target != null) {
+            target.textContent = txt;
+        }
+
+        return target;
+    }
+
+    setAll(key, txt) {
+        const targets = this._allByClass(key);
+
+        for (let e of targets) {
+            e.textContent = txt;
+        }
+
+        return targets;
+    }
+}
--- Challenge08/weather.js
+++ Challenge08/weather.js
@@ -1,13 +1,14 @@
 import { api_key } from './key.js';
 import { daysOfWeekMap } from './conf.js';
 import { GeoLocation } from './geolocation.js';
+import { DOM } from './dom.js';
 import { SVG } from './svg.js';
 
 export class Weather {
     static days = 7;
     static owm_api = `https://api.openweathermap.org/data/2.5/onecall`;
 
-    dom_element = document.createElement('ol');
+    dom_element = document.querySelector('#app ol');
 
     forecasts = new Array();
 
@@ -83,7 +84,8 @@ export class Weather {
 }
 
 class WeatherData {
-    dom_element = document.createElement('li');
+    dom_template = document.querySelector('#app ol li.template');
+    dom_element = this.dom_template.cloneNode(true);
 
     date;
     weather;
@@ -92,14 +94,30 @@ class WeatherData {
     tempeature_feel;
 
     constructor(data) {
-        this.date = new Date(Number(data.dt) * 1000);
+        this.date = new Date(data.dt * 1000);
         this.weather = data.weather[0].main;
         this.tempeature = data.temp.day;
-        this.precipitation = Number(data.pop) * 100;
+        this.precipitation = data.pop * 100;
         this.tempeature_feel = data.feels_like.day;
+
+        const node = new DOM(this.dom_element);
+        node.dot('weather').classList.add(this.weather);
+        node.dot('week').textContent = daysOfWeekMap[this.date.getDay()];
+        node.dot('date').textContent = this.date.getDate();
+        node.dot('template p').textContent = this.tempeature.toFixed(0);
+        node.dot('precipitation span').textContent =
+            this.precipitation.toFixed(0);
+        node.dot('temp-feel span').textContent =
+            this.tempeature_feel.toFixed(0) + '°';
+
+        this.dom_element.classList.remove('template');
     }
 
     render() {
+        return this.dom_element;
+    }
+
+    render02() {
         const svg_low = new SVG('low-svg');
         const svg_preci = new SVG('preci-svg');
 
@@ -114,7 +132,7 @@ class WeatherData {
                 </div>
                 <div class="precipitation cloudy-text">
                     ${svg_preci.sources}<span
-                        >${this.precipitation}%</span
+                        >${this.precipitation.toFixed(0)}%</span
                     >
                 </div>
                 <div class="temp-feel cloudy-text">
