d8c8b06ded2b0cf1af137b53686f221119746ebf
Jimmy NGAI <jimmyngai@ssm.gov.mo>
2022-03-30 10:31:31 +0800

    Refactoring lazydom again, changed to correct private method declaratoion, create lazy DOM object from element, and polish the lazy initialization flow.

--- Challenge09/main.js
+++ Challenge09/main.js
@@ -4,7 +4,7 @@ import { contents } from './data.js';
 class Main extends DOM {
     static imgdir = 'images';
 
-    thumbnails = document.querySelector('#app .thumbnails');
+    thumbnails = new DOM('#app .thumbnails');
     featured_img = document.querySelector('#app img.featured');
     featured_cap = document.querySelector('#app p.caption');
 
@@ -15,28 +15,28 @@ class Main extends DOM {
         super('#app');
 
         this.contents = data;
-        this.idx_maximum = this.contents.length;
+        this.idx_maximum = data.length;
 
         this.render();
-        this._bindEvents();
+        this.#bindEvents();
     }
 
-    _imgUrl(img) {
+    #imgUrl(img) {
         return `${Main.imgdir}/${img}`;
     }
 
     updateImg = (e) => {
-        this.featured_img.setAttribute('src', this._imgUrl(e.target.image));
+        this.featured_img.setAttribute('src', this.#imgUrl(e.target.image));
         this.featured_cap.textContent = e.target.caption + '.';
 
         this.current_idx = Number(e.target.value);
     };
 
-    _advenceImg(step) {
+    #advenceImg(step) {
         this.current_idx = (this.current_idx + step) % this.idx_maximum;
         if (this.current_idx < 0) this.current_idx += this.idx_maximum;
 
-        const thumbnail = this.thumbnails.querySelector(
+        const thumbnail = this.thumbnails.q(
             `input[value="${this.current_idx}"]`
         );
         thumbnail.click();
@@ -44,14 +44,14 @@ class Main extends DOM {
     }
 
     clickLeft = (e) => {
-        this._advenceImg(-1);
+        this.#advenceImg(-1);
     };
 
     clickRight = (e) => {
-        this._advenceImg(+1);
+        this.#advenceImg(+1);
     };
 
-    _bindEvents(elements) {
+    #bindEvents(elements) {
         const left_buttons = document.querySelector('#app img.screenbtn.left');
         const right_buttons = document.querySelector(
             '#app img.screenbtn.right'
@@ -63,9 +63,9 @@ class Main extends DOM {
     render() {
         for (let i in this.contents) {
             const c = this.contents[i];
-            const img = new DOM('label.lazydom', this.thumbnails);
+            const img = this.thumbnails.dom('label.lazydom');
             img.dot([
-                ['img', '', { src: this._imgUrl(c.image), alt: c.caption }],
+                ['img', '', { src: this.#imgUrl(c.image), alt: c.caption }],
                 ['input', '', { value: i, change: this.updateImg }],
             ]);
 
@@ -74,7 +74,7 @@ class Main extends DOM {
             inp.caption = c.caption;
         }
 
-        this.thumbnails.querySelector('input:first-child')?.click();
+        this.thumbnails.q('input:first-child')?.click();
     }
 }
 
--- Challenge09/util/lazydom.js
+++ Challenge09/util/lazydom.js
@@ -9,43 +9,42 @@ export class DOM {
     classList;
 
     constructor(selector = `.${DOM.template_placeholder}`, parent_node = document) {
-        const template_element = this._lookup(selector, parent_node);
-        this.element = template_element.cloneNode(true);
-
-        if (parent_node != document) {
-            parent_node.append(this.element);
-        }
-
-        this._styleShorthand();
-        this._cleanUp(template_element);
+        const template_element = this.#lookup(selector, parent_node);
+        (parent_node != document) 
+            ? this.#cloneElement(template_element, parent_node)
+            : this.element = template_element;
+        this.#styleShorthand();
     }
 
-    /*
     static fromElement(element) {
-        const new_dom = new DOM();
-        new_dom.element = element;
-        this._styleShorthand();
-        return new_dom;
-    }
-    */
-
-    _styleShorthand() {
-        this.style = this.element.style;
-        this.classList = this.element.classList;
+        const n = new DOM();
+        //Updating the element and its shorthand
+        n.element = element;
+        n.#styleShorthand();
+        return n;
     }
 
-    _cleanUp(template_element) {
+    #styleShorthand() {
+        this.style = this.element?.style;
+        this.classList = this.element?.classList;
+    }
+
+    #cloneElement(template_element, parent_node) {
+        this.element = template_element.cloneNode(true);
+        parent_node.append(this.element);
+        this.#cleanUp(template_element);
+    }
+
+    #cleanUp(template_element) {
         //Remove the duplicated class which cloned from template
-        if (this.classList.contains(DOM.template_placeholder)) this.classList.remove(DOM.template_placeholder);
-
+        if (this.element.classList.contains(DOM.template_placeholder)) this.element.classList.remove(DOM.template_placeholder);
         //We could hide the template by default, show our element after cloned
-        if (this.style.display === 'none') this.style.display = '';
-
+        if (this.element.style.display === 'none') this.element.style.display = '';
         //Remove the lazy template element from dom tree to save our time
         if (template_element?.classList.contains(DOM.template_placeholder)) template_element.remove();
     }
 
-    _lookup(selector, parent_node) {
+    #lookup(selector, parent_node) {
         if ((parent_node in DOM.CACHE) && (selector in DOM.CACHE[parent_node])) {
             return DOM.CACHE[parent_node][selector];
         } else {
@@ -81,20 +80,16 @@ export class DOM {
         if (target_node != null) target_node.textContent = value;
     }
 
-    dotText(texts) {
-        for (let text of texts) {
-            this._setTextContent(...text.slice(0, 2));
-        }
-    }
-
     dot(dataset) {
         for (let d of dataset) {
+            //If the second elemenmt is not empty text, set the textContent
             if (d.length >= 2 && d[1] != '') this.text(d[0], d[1]);
+            //If there is attributes object, assign it
             if (d.length == 3) this.attr(d[0], d[2]);
         }
     }
 
-    dom(selector) {
-        alert();
+    dom(selector = `.${DOM.template_placeholder}`) {
+        return new DOM(selector, this.element);
     }
 }
