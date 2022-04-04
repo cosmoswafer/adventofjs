6cbeae0d889eb469a0e9e86842573533744da5af
Jimmy NGAI <jimmyngai@ssm.gov.mo>
2022-04-04 18:23:50 +0800

    Generate the days

--- Challenge14/main.js
+++ Challenge14/main.js
@@ -1,6 +1,34 @@
-import './style.css'
+import { DOM } from './util/lazydom.js';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+class CalendarView {
+    today = new Date();
+
+    constructor(parent_element) {
+        this.calendar = parent_element.DOM('.calendar');
+        console.dir(this.today);
+        console.log(this.today.toISOString());
+
+        this.#buildCalendar();
+    }
+
+    #buildCalendar() {
+        const offset = 2;
+        for (let i = 0; i < 7 * 5; i++) {
+            const day = this.calendar.DOM();
+            const d = i - offset;
+            day.element.textContent =
+                d > 0 && d <= 31 ? d : '_';
+            if (d == this.today.getDate()) day.classList.add('today');
+        }
+    }
+}
+
+class Main {
+    app = new DOM('#app');
+
+    constructor() {
+        this.calendar = new CalendarView(this.app);
+    }
+}
+
+const main = new Main();
