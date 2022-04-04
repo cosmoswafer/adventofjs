52a1914e80c1578be63958f0d364bb7b514e1bf3
Jimmy NGAI <jimmyngai@ssm.gov.mo>
2022-04-04 13:10:43 +0800

    Added the modal toggle logic

---
+++ Challenge13/main.js
@@ -0,0 +1,31 @@
+import { DOM } from './util/lazydom.js';
+
+class ModalDiv {
+    #display_value = '';
+
+    constructor(parent_element) {
+        this.modal = parent_element.DOM('.modal');
+        this.#display_value = this.modal.style.display;
+
+        this.modal.q('img.close').addEventListener('click', (e) => this.toggle());
+    }
+
+    toggle() {
+        const invisible_code = 'none';
+        const hiddenness = this.modal.style.display === invisible_code;
+        this.modal.style.display = hiddenness
+            ? this.#display_value
+            : invisible_code;
+    }
+}
+
+class Main {
+    app = new DOM('#app');
+    modal = new ModalDiv(this.app);
+
+    constructor() {
+        //this.app.q('img.background').addEventListener('click', (e) => this.modal.toggle());
+    }
+}
+
+const main = new Main();
