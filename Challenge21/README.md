0dcc8be9be29ae6f787ae6a39c27b98131896ca2
Jimmy <jimmy09140456@gmail.com>
2022-06-12 22:42:18 +0800

    Finished Challange 21

---
+++ Challenge21/README.md
@@ -0,0 +1,27 @@
+# Day 21 - Simplified Budget
+
+![Advent of JavaScript](screen.gif)
+
+## Challenge
+
+**Users should be able to:**
+
+-   The user should be able to enter a budget amount. As soon as they leave the budget amount input field (blur), the summary information should update
+-   The user should be able to ender a vendor and an amount into the input fields. When they click on the plus button, the amount should get added to the table on the expenses table on the right.
+-   When an expense is added, the summary information should be updated
+-   The user should be able to click on the delete an expense button and the row will be removed
+-   When an expense is removed, the summary information should be updated
+-   The summary information in the footer should display the income, expenses, and balance
+-   If the balance is a positive number, the text should be green.
+-   If the balance is a negative number, the text should be red.
+
+## Write-up
+
+### CSS
+
+-
+
+### JavaScript
+
+-
+
---
+++ Challenge21/main.js
@@ -0,0 +1,153 @@
+import { DOM } from 'https://jimmy09140456.github.io/lazydom/dom.js';
+
+class Util {
+    static usdFormatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    static currencyFormatter(n) {
+        return Util.usdFormatter.format(n);
+    }
+}
+
+class Budget {
+    budgetAmountInput = document.querySelector('#budget-amount');
+    budgetIncomeTxt = document.querySelector(
+        '#app > .balance .income .currency'
+    );
+
+    amount = 0;
+
+    constructor(f) {
+        this.notifier = f;
+        this.#bindEvents();
+        //Reset amount
+        this.renderIncome();
+    }
+
+    #bindEvents() {
+        this.budgetAmountInput.addEventListener(
+            'change',
+            this.changeBudgetAmount
+        );
+    }
+
+    changeBudgetAmount = () => {
+        this.amount = Number(this.budgetAmountInput.value);
+        this.renderIncome();
+        this.notifier();
+    };
+
+    renderIncome() {
+        this.budgetIncomeTxt.textContent = Util.currencyFormatter(this.amount);
+    }
+}
+
+class Expense {
+    expenseUl = new DOM('#app > .expenses ul.items');
+    expenseName = document.querySelector('#expense-name');
+    expenseAmount = document.querySelector('#expense-amount');
+    addExpenseBtn = document.querySelector('#app > .budget .add-btn a');
+    totalExpenseText = document.querySelector(
+        '#app > .balance .expenses .currency'
+    );
+
+    expenseItems = new Array();
+
+    constructor(f) {
+        this.notifier = f;
+        this.#bindEvents();
+        this.#cleanUp();
+    }
+
+    #cleanUp() {
+        //Start with an empty list
+        const firstItem = this.expenseUl.DOM();
+        firstItem.element.remove();
+        //Reset Expenses
+        this.renderExpenses();
+    }
+
+    #bindEvents() {
+        this.addExpenseBtn.addEventListener('click', () =>
+            this.renderNewItem()
+        );
+    }
+
+    renderNewItem = () => {
+        const nextItemKey = this.expenseItems.length;
+        const newItem = this.expenseUl.DOM();
+        newItem.dot(
+            ['.name', this.expenseName.value],
+            ['.amount', Util.currencyFormatter(this.expenseAmount.value)]
+        );
+        newItem
+            .q('.trash a')
+            .addEventListener('click', () => this.removeItem(nextItemKey));
+        this.expenseItems.push({
+            key: nextItemKey,
+            dom: newItem,
+            amount: Number(this.expenseAmount.value),
+            isTrash: false,
+        });
+        console.log('Added new item at', nextItemKey);
+
+        this.renderExpenses();
+        this.notifier();
+    };
+
+    removeItem(key) {
+        const item = this.expenseItems[key];
+        if (item.key === key) {
+            //It shouldn't be false
+            item.dom.element.remove();
+            item.isTrash = true;
+        }
+
+        this.renderExpenses();
+        this.notifier();
+    }
+
+    renderExpenses() {
+        let totalAmount = 0;
+        for (let i of this.expenseItems) {
+            if (i.isTrash === false) {
+                totalAmount += i.amount;
+            }
+        }
+        this.totalExpenseText.textContent = Util.currencyFormatter(totalAmount);
+    }
+}
+
+class App {
+    balanceText = document.querySelector('#app > .balance .balance .currency');
+
+    constructor() {
+        this.budget = new Budget(this.notify);
+        this.expense = new Expense(this.notify);
+        //Reset balance text
+        this.renderBalance(0);
+    }
+
+    notify = () => {
+        console.log('Render balance...');
+        console.log('Budget amount:', this.budget.amount);
+        let balance = this.budget.amount;
+        for (let i of this.expense.expenseItems) {
+            console.log(`${i.key}: ${i.amount}`);
+            if (i.isTrash === false) {
+                balance -= i.amount;
+            }
+        }
+        console.log({ balance });
+        this.renderBalance(balance);
+    };
+
+    renderBalance(n) {
+        this.balanceText.textContent = Util.currencyFormatter(n);
+        this.balanceText.classList.remove('positive', 'negative');
+        n >= 0 ? this.balanceText.classList.add('positive') : this.balanceText.classList.add('negative');
+    }
+}
+
+const app = new App();
